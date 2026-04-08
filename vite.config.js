757e699ea4f39697cf71fede53164c24df0e071c
HitenSan <prajapati.hiten05@gmail.com>
2026-04-08 20:29:41 +0530

    fix name of the repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Rose_day3d_website/",
+  base: "/RoseDay_3Dweb/",
 })
